df05769d4acccf449cb182696bf982601503d3b5
Junyi <365154018@qq.com>
2018-10-31 14:23:47 +0800

    Merge branch 'master' of C:\Users\li\PycharmProjects\blockchain with conflicts.

---
+++ spider/requests_exceptions.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+
+@file: requests_exceptions.py
+@time: 2018/10/24 11:19
+Created by Junyi.
+"""
---
+++ spider/requests_handler.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+
+@file: requests_handler.py
+@time: 2018/10/24 11:19
+Created by Junyi.
+"""
